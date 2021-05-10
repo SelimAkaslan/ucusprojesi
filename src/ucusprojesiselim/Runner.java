387976ecf2346ed46bcad2940e9b6814ea721d46
selim akaslan <sakaslan6957@gmail.com>
2021-05-11 00:14:30 +0200

    ucus first commit

---
+++ src/ucusprojesiselim/Runner.java
@@ -0,0 +1,9 @@
+package ucusprojesiselim;
+
+public class Runner {
+    public static void main(String[] args) {
+    ucusBilgileri ucus = new ucusBilgileri();
+    System.out.println("Ucus firmamiza hosgeldiniz!");
+    ucus.menu();
+}
+}
